7cdc04b3e04f40f52b4c5d1c90f67974bcf8fd27
Pokesalot <athankinson@gmail.com>
2018-11-05 16:59:32 -0500

    (edit) changed the file to mirror the tutorial

--- OOPOutlookTestAddin/OOPOutlookTestAddinWeb/MessageRead.js
+++ OOPOutlookTestAddin/OOPOutlookTestAddinWeb/MessageRead.js
@@ -1,90 +1,31 @@
-﻿(function () {
-  "use strict";
+﻿'use strict';
 
-  var messageBanner;
+(function () {
 
-  // The Office initialize function must be run each time a new page is loaded.
-  Office.initialize = function (reason) {
-    $(document).ready(function () {
-      var element = document.querySelector('.ms-MessageBanner');
-      messageBanner = new fabric.MessageBanner(element);
-      messageBanner.hideBanner();
-      loadProps();
-    });
-  };
+    // Initialization will fail if Office.initialize
+    // does not have a function assigned to it
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            loadItemProps(Office.context.mailbox.item);
+        });
+    };
 
-  // Take an array of AttachmentDetails objects and build a list of attachment names, separated by a line-break.
-  function buildAttachmentsString(attachments) {
-    if (attachments && attachments.length > 0) {
-      var returnString = "";
-      
-      for (var i = 0; i < attachments.length; i++) {
-        if (i > 0) {
-          returnString = returnString + "<br/>";
-        }
-        returnString = returnString + attachments[i].name;
-      }
+    function loadItemProps(item) {
+        // Get the table body element
+        var tbody = $('.prop-table');
 
-      return returnString;
+        // Add a row to the table for each message property
+        tbody.append(makeTableRow("Id", item.itemId));
+        tbody.append(makeTableRow("Subject", item.subject));
+        tbody.append(makeTableRow("Message Id", item.internetMessageId));
+        tbody.append(makeTableRow("From", item.from.displayName + " &lt;" +
+            item.from.emailAddress + "&gt;"));
     }
 
-    return "None";
-  }
-
-  // Format an EmailAddressDetails object as
-  // GivenName Surname <emailaddress>
-  function buildEmailAddressString(address) {
-    return address.displayName + " &lt;" + address.emailAddress + "&gt;";
-  }
-
-  // Take an array of EmailAddressDetails objects and
-  // build a list of formatted strings, separated by a line-break
-  function buildEmailAddressesString(addresses) {
-    if (addresses && addresses.length > 0) {
-      var returnString = "";
-
-      for (var i = 0; i < addresses.length; i++) {
-        if (i > 0) {
-          returnString = returnString + "<br/>";
-        }
-        returnString = returnString + buildEmailAddressString(addresses[i]);
-      }
-
-      return returnString;
+    function makeTableRow(name, value) {
+        return $("<tr><td><strong>" + name +
+            "</strong></td><td class=\"prop-val\"><code>" +
+            value + "</code></td></tr>");
     }
 
-    return "None";
-  }
-
-  // Load properties from the Item base object, then load the
-  // message-specific properties.
-  function loadProps() {
-    var item = Office.context.mailbox.item;
-
-    $('#dateTimeCreated').text(item.dateTimeCreated.toLocaleString());
-    $('#dateTimeModified').text(item.dateTimeModified.toLocaleString());
-    $('#itemClass').text(item.itemClass);
-    $('#itemId').text(item.itemId);
-    $('#itemType').text(item.itemType);
-
-    $('#message-props').show();
-
-    $('#attachments').html(buildAttachmentsString(item.attachments));
-    $('#cc').html(buildEmailAddressesString(item.cc));
-    $('#conversationId').text(item.conversationId);
-    $('#from').html(buildEmailAddressString(item.from));
-    $('#internetMessageId').text(item.internetMessageId);
-    $('#normalizedSubject').text(item.normalizedSubject);
-    $('#sender').html(buildEmailAddressString(item.sender));
-    $('#subject').text(item.subject);
-    $('#to').html(buildEmailAddressesString(item.to));
-  }
-
-  // Helper function for displaying notifications
-  function showNotification(header, content) {
-    $("#notificationHeader").text(header);
-    $("#notificationBody").text(content);
-    messageBanner.showBanner();
-    messageBanner.toggleExpansion();
-  }
 })();
