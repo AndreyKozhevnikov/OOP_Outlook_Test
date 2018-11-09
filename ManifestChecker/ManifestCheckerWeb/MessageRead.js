93a5d08079938bac9f1a9b761df1aa2de4cebec3
Pokesalot <athankinson@gmail.com>
2018-11-09 15:50:13 -0500

    (update) The thing works!
    It works so hard! This is the most work that has ever been worked by something I've made that actually works!

--- ManifestChecker/ManifestCheckerWeb/MessageRead.js
+++ ManifestChecker/ManifestCheckerWeb/MessageRead.js
@@ -1,32 +1,33 @@
 ﻿var item;
 
+var recipient = [{
+    "displayName": "OOPIT Level 1",
+    "emailAddress": "oopit@psu.edu"
+}];
+
 (function () {
     "use strict";
-
-    var messageBanner;
     
 
     Office.initialize = function () {
         item = Office.context.mailbox.item;
-        // Checks for the DOM to load using the jQuery ready function.
         $(document).ready(function () {
-            // After the DOM is loaded, app-specific code can run.
-            // Insert data in the top of the body of the composed 
-            // item.
-            //prependItemBody();
-            //Office.context.mailbox.item.to.setAsync("This is to whom");
-
             
         });
-        Office.context.mailbox.item.body.prependAsync("This is done from Read Initialize test!");
 
         $("#fauxButton").click(function () {
-            Office.context.mailbox.item.body.prependAsync("This is done from Read Initialize button test!");
+            Office.context.mailbox.item.to.setAsync(recipient);
+            Office.context.mailbox.item.subject.setAsync("This is a subject!")
+            Office.context.mailbox.item.body.setAsync("This is done from Read Initialize button test!");
+        });
+
+        $("#button2").click(function () {
+            Office.context.mailbox.item.to.setAsync(recipient);
+            Office.context.mailbox.item.subject.setAsync("This is a subject also!")
+            Office.context.mailbox.item.body.setAsync("This is done from Read Initialize button 2 test!");
         });
     }
-
-    //Throws an error
-    //Office.context.mailbox.item.body.prependAsync("This is done from Read Function!");
+    
 })();
 
 
