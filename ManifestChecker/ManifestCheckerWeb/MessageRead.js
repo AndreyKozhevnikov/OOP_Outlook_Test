e7d5bb28b50b3ed08d79b4e1d8a4234ef76120f9
Pokesalot <athankinson@gmail.com>
2018-11-12 14:00:50 -0500

    (update) WORKING ADD-IN
    Starting to play with buttons that make other buttons, saving a working version before we dig too deep on that.

--- ManifestChecker/ManifestCheckerWeb/MessageRead.js
+++ ManifestChecker/ManifestCheckerWeb/MessageRead.js
@@ -15,25 +15,36 @@ var recipient = [{
             
         });
 
-        
+        /*
+         * This is some code!
+         * 
+         * Entries are written as jQuery statements. More recipients can be added to the recipients variable above.
+         * 
+         * Functionality can be added to any HTML in MessageRead.html so long as the element has a guid.
+         * The jQuery functionality is copy-paste, and any html elements can be added in place.
+         * 
+        */
 
         $("#fauxButton").click(function () {
             Office.context.mailbox.item.to.setAsync(recipient);
             Office.context.mailbox.item.subject.setAsync("This is a subject!")
-            Office.context.mailbox.item.body.setAsync("This is done from Read Initialize button test!");
+            Office.context.mailbox.item.body.setAsync("This is done from<br /> Read Initialize button test!", { coercionType: Office.CoercionType.Html });
         });
 
         $("#button2").click(function () {
+
+            
             Office.context.mailbox.item.to.setAsync(recipient);
             Office.context.mailbox.item.subject.setAsync("This is a subject also!")
-            Office.context.mailbox.item.body.setAsync("This is done from Read Initialize button 2 test!");
+            Office.context.mailbox.item.body.setAsync("This is done from Read Initialize button 2 test!", { coercionType: Office.CoercionType.Html });
         });
 
         $("#realButton").click(function () {
             Office.context.mailbox.item.to.setAsync(recipient);
             Office.context.mailbox.item.subject.setAsync("This is a subject also but from a real button!")
-            Office.context.mailbox.item.body.setAsync("This is done from Read Initialize button 1asgdfasdfasd test!");
+            Office.context.mailbox.item.body.setAsync("This is done from Read Initialize button 1asgdfasdfasd test!", { coercionType: Office.CoercionType.Html });
         });
+
     }
     
 })();
